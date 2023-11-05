98e78cb75a6bd991e774e0696646e276ebc1ece3
Ken <ken205115331@gmail.com>
2023-11-06 01:06:43 +0800

    conda script

---
+++ docker.sh
@@ -0,0 +1,11 @@
+#https://devopscube.com/how-to-install-and-configure-docker/
+sudo apt-get update -y
+sudo apt-get install software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository -y \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+sudo docker version
